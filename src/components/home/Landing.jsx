4a4643fd9d724800e8412b74766d363782f8dfb7
Mostafa-m-gomaa <aagomaaa2@gmail.com>
2024-07-10 07:12:35 +0300

    mostafa

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import VideoBackground from './VideoBackground '
+import { WavyBackground } from '../wavy-background'
 
 const Landing = () => {
   return (
     <div className='w-full h-[100vh] relative  bg-black'>
-      <VideoBackground  />
+      {/* <VideoBackground  /> */}
+      <WavyBackground />
       
       <div className="flex flex-col text-white w-[90%] gap-y-3 top-[40%] left-[50%] translate-x-[-50%] absolute bottom-0 z-10 sm:w-[50%] sm:text-center">
         <div className='text-[30px] sm:text-[45px]'>Spring / Summer / 2024</div>
